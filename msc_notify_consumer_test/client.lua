fb397b8be4264419a2cf573dca53b5ec4c7a61d3
ututu <colonel.nyaa@gmail.com>
2026-05-27 13:48:30 +0900

    Add notify consumer adapter guard

--- msc_notify_consumer_test/client.lua
+++ msc_notify_consumer_test/client.lua
@@ -1,5 +1,21 @@
+local notifyAdapterResource = 'msc_notify_adapter_test'
+
+local function showNativeNotification(message)
+    BeginTextCommandThefeedPost('STRING')
+    AddTextComponentSubstringPlayerName(message)
+    EndTextCommandThefeedPostTicker(false, false)
+end
+
 RegisterCommand('consumernotify', function()
-    exports['msc_notify_adapter_test']:notify({
+    if GetResourceState(notifyAdapterResource) ~= 'started' then
+        local message = 'Notify adapter is not started.'
+
+        showNativeNotification(message)
+        print(('[msc_notify_consumer_test] %s Start %s before using /consumernotify.'):format(message, notifyAdapterResource))
+        return
+    end
+
+    exports[notifyAdapterResource]:notify({
         title = 'Consumer Test',
         description = 'This notification was called from another resource.',
         type = 'inform'
